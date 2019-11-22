c3cd9ed621039ab2fd9c611365577218c86a7bf9
Ri507 <58082637+Ri507@users.noreply.github.com>
2019-11-22 11:05:04 -0500

    Add files via upload

---
+++ RichardsonRAreaPython2.py
@@ -0,0 +1,25 @@
+import random
+
+l = random.randint(1,20)
+
+print(l)
+
+w = random.randint(1,20)
+
+print(w)
+
+print(l*w)
+
+room = random.randint(1,20)
+
+print(room)
+
+if room >= 100:
+    print("huge")
+elif room <= 100:
+    print("small")
+if room == 100:
+    print("its just right")
+
+
+          
